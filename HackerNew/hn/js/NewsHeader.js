a2064a6a59e17ee17a41e72fcb57e6a1b4791b3e
ittus <vuminhthang.cm@gmail.com>
2015-11-10 23:26:30 +0800

    Add header and navigation bar

--- HackerNew/hn/js/NewsHeader.js
+++ HackerNew/hn/js/NewsHeader.js
@@ -1,6 +1,6 @@
 var React = require('react');
 var $ = require('jquery');
-
+var _ = require('lodash');
 var NewsHeader = React.createClass({
 	getLogo: function(){
 		return (
@@ -16,11 +16,59 @@ var NewsHeader = React.createClass({
 			</div>
 		);
 	},
+	 getNav: function () {
+    var navLinks = [
+      {
+        name: 'new',
+        url: 'newest'
+      },
+      {
+        name: 'comments',
+        url: 'newcomments'
+      },
+      {
+        name: 'show',
+        url: 'show'
+      },
+      {
+        name: 'ask',
+        url: 'ask'
+      },
+      {
+        name: 'jobs',
+        url: 'jobs'
+      },
+      {
+        name: 'submit',
+        url: 'submit'
+      }
+    ];
+     return (
+      <div className="newsHeader-nav">
+        {_(navLinks).map(function (navLink) {
+          return (
+            <a key={navLink.url} className="newsHeader-navLink newsHeader-textLink" href={'https://news.ycombinator.com/' + navLink.url}>
+              {navLink.name}
+            </a>
+          );
+        }).value()}
+      </div>
+    );
+ 	},
+ 	getLogin: function () {
+    return (
+      <div className="newsHeader-login">
+        <a className="newsHeader-textLink" href="https://news.ycombinator.com/login?whence=news">login</a>
+      </div>
+    );
+  },
 	render: function() {
 		return (
 			<div className="newsHeader">
 		        {this.getLogo()}
 		        {this.getTitle()}
+		        {this.getNav()}
+		        {this.getLogin()}
       		</div>
 		);
 	}
