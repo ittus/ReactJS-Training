d4b817020ba5ca0972076568444de8ccd7547a69
ittus <vuminhthang.cm@gmail.com>
2015-11-10 23:37:51 +0800

    finish demo with ajax request

---
+++ HackerNew/hn/js/app.js
@@ -0,0 +1,27 @@
+var _ = require('lodash');
+var $ = require('jquery');
+var NewsList = require('./NewsList');
+var React = require('react');
+var ReactDOM = require('react-dom')
+// Get the top item ids
+$.ajax({
+  url: 'https://hacker-news.firebaseio.com/v0/topstories.json',
+  dataType: 'json'
+}).then(function (stories) {
+  // Get the item details in parallel
+  var detailDeferreds = _(stories.slice(0, 30)).map(function (itemId) {
+    return $.ajax({
+      url: 'https://hacker-news.firebaseio.com/v0/item/' + itemId + '.json',
+      dataType: 'json'
+    });
+  }).value();
+  return $.when.apply($, detailDeferreds);
+}).then(function () {
+  // Extract the response JSON
+  var items = _(arguments).map(function (argument) {
+    return argument[0];
+  }).value();
+
+  // Render the items
+  ReactDOM.render(<NewsList items={items}/>, $('#content')[0]);
+});
